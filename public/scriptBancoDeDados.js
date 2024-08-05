b1f27b2271716c6f75dad2d5f941837055eeb801
Glitch (gerador-de-contratos-sebraetec) <none>
2024-08-05 17:19:03 +0000

    📟🐉 Checkpoint ./serverBD.js:38290350/5433 ./packageBD.json:38290350/58 ./public/scriptBancoDeDados.js:38290350/39

--- public/scriptBancoDeDados.js
+++ public/scriptBancoDeDados.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const buscaCep = require("busca-cep");
 const mysql = require("mysql");
 
 app.use(bodyParser.json());
