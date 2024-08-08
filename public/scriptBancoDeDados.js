4e1c79f1b987fd96b008eceed3a181bca232abda
Glitch (gerador-de-contratos-sebraetec) <none>
2024-08-08 15:13:48 +0000

    ☺️🏌 Checkpoint ./public/scriptBancoDeDados.js:38290350/10470

--- public/scriptBancoDeDados.js
+++ public/scriptBancoDeDados.js
@@ -17,6 +17,82 @@ const botaoLimparDados = document.querySelector("#botaoLimparDados")
 
 let listaDeDados = []
 
+botaoAddDados.addEventListener("click", function() {
+  // Capturar os valores dos campos de entrada
+  let nomeBD = entradaDeNome.value;
+  let cpfBD = entradaDeCpf.value;
+  let emailBD = entradaDeEmail.value;
+  let telBD = entradaDeTel.value;
+  let enderecoBD = entradaDeEnd.value;
+
+  // Exibir os valores capturados no console para verificação
+  console.log("Dados capturados:");
+  console.log("Nome:", nomeBD);
+  console.log("CPF:", cpfBD);
+  console.log("E-mail:", emailBD);
+  console.log("Telefone:", telBD);
+  console.log("Endereço:", enderecoBD);
+
+  // Adicionar os valores ao array 'values'
+  var values = [
+    [nomeBD, cpfBD, emailBD, telBD, enderecoBD]
+  ];
+
+  var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB
+});
+  
+  // Estabelecer a conexão com o banco de dados e inserir os dados
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO ClientesSEBRAE (nome, cpf, email, telefone, endereco) VALUES ?";
+    
+    con.query(sql, [values], function (err, result) {
+      if (err) throw err;
+      console.log("Number of records inserted: " + result.affectedRows);
+    });
+  });
+
+  // Atualizar a lista visual com os dados
+  for (let i=0;i<listaDeDados.length;i=i+1){
+    console.log(listaDeDados[i])
+    futuroValorInnerHTML=futuroValorInnerHTML+"<li>"+listaDeDados[i]+"</li>"
+  }
+  
+  console.log(futuroValorInnerHTML)
+  
+  listaMontada.innerHTML=futuroValorInnerHTML
+  
+  })
+
+
+
+
+/*
+---------------------------------------------------------------
+const entradaDeNome = document.querySelector("#nomeCon")
+const entradaDeCpf = document.querySelector("#cpfCon")
+const entradaDeEmail = document.querySelector("#emailCon")
+const entradaDeTel = document.querySelector("#telCon")
+const entradaDeEnd = document.querySelector("#enderecoCon")
+console.log(entradaDeNome)
+
+const botaoAddDados = document.querySelector("#botaoAdicionarDados")
+console.log(botaoAddDados)
+
+const listaMontada = document.querySelector("#listaVisualDeDados")
+console.log(listaMontada)
+
+const botaoLimparDados = document.querySelector("#botaoLimparDados")
+
+let listaDeDados = []
+
 botaoAddDados.addEventListener("click",function(){
   console.log(entradaDeNome.value)
   listaDeDados.push(entradaDeNome.value)
@@ -38,7 +114,11 @@ botaoAddDados.addEventListener("click",function(){
   listaMontada.innerHTML=futuroValorInnerHTML
   
   })
+--------------------------------------------------------------------------
+*/
 
+/*
+--------------------------------------------------
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
@@ -69,7 +149,8 @@ botaoLimparDados.addEventListener("click",function(){
     listaMontada.innerHTML=""
     listaDeDados=[]
   })
-
+-------------------------------------------------------------
+*/
 
 /*
 -----------------------------
