b5828e8044098074d0d311d4dcf1f33b2e41ead4
Glitch (gerador-de-contratos-sebraetec) <none>
2024-08-08 02:24:26 +0000

    💪🙋 Checkpoint ./public/scriptBancoDeDados.js:38290350/2322

--- public/scriptBancoDeDados.js
+++ public/scriptBancoDeDados.js
@@ -1,5 +1,13 @@
 var mysql = require('mysql');
 
+document.getElementById('enviaBD').addEventListener('click', async function() {
+  const nomeBD = document.getElementById('nome').value;
+  const cpfBD = document.getElementById('cpf').value;
+  const emailBD = document.getElementById('email').value;
+  const telBD = document.getElementById('tel').value;
+  const enderecoBD = document.getElementById('endereco').value;
+
+
 var con = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
@@ -10,27 +18,12 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "INSERT INTO ClientesSEBRAE (nome, cpf, email, telefone, endereco) VALUES ('Company Inc', 'Highway 37', 'TEste', 'testando', 'apenas Teste')";
-  con.query(sql, function (err, result) {
+  var sql = "INSERT INTO customers (name, address) VALUES ?";
+  var values = 
+    ['${nomeBD}', '${cpfBD}', '${emailBD}', '${telBD}', '${enderecoBD}'],
+  
+  con.query(sql, [values], function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
   });
 });
-
-
-
-document.getElementById('enviaBD').addEventListener('click', async function() {
-  const nomeBD = document.getElementById('nome').value;
-  const cpfBD = document.getElementById('cpf').value;
-  const emailBD = document.getElementById('email').value;
-  const telBD = document.getElementById('tel').value;
-  const enderecoBD = document.getElementById('endereco').value;
-
-
-
-    // Preenchendo a tabela
-    document.getElementById('cnpj-td').textContent = cnpjDigitado;
-    document.getElementById('razao-social-td').textContent = dataCnpj.nome;
-    document.getElementById('empresa-atividade-principal').textContent = dataCnpj.atividade_principal[0].text;
-    document.getElementById('empresa-nome-fantasia').textContent = dataCnpj.fantasia;
-    document.getElementById('empresa-logradouro').textContent = dataCnpj.logradouro;
