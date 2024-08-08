1043f6f43ca7d04cd8228ef5664769076767ea4f
Glitch (gerador-de-contratos-sebraetec) <none>
2024-08-08 02:13:16 +0000

    ⛰🚍 Checkpoint ./public/scriptBancoDeDados.js:38290350/2005 ./public/bancoDeDados.html:38290350/334

--- public/scriptBancoDeDados.js
+++ public/scriptBancoDeDados.js
@@ -16,3 +16,21 @@ con.connect(function(err) {
     console.log("1 record inserted");
   });
 });
+
+
+
+document.getElementById('enviaBD').addEventListener('click', async function() {
+  const nomeBD = document.getElementById('nome').value;
+  const cpfBD = document.getElementById('cpf').value;
+  const emailBD = document.getElementById('email').value;
+  const telBD = document.getElementById('tel').value;
+  const enderecoBD = document.getElementById('endereco').value;
+
+
+
+    // Preenchendo a tabela
+    document.getElementById('cnpj-td').textContent = cnpjDigitado;
+    document.getElementById('razao-social-td').textContent = dataCnpj.nome;
+    document.getElementById('empresa-atividade-principal').textContent = dataCnpj.atividade_principal[0].text;
+    document.getElementById('empresa-nome-fantasia').textContent = dataCnpj.fantasia;
+    document.getElementById('empresa-logradouro').textContent = dataCnpj.logradouro;
