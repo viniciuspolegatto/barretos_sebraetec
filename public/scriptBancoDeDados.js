305cb546f83d0588501f3daaac0259656712ca1a
Glitch (gerador-de-contratos-sebraetec) <none>
2024-08-08 01:39:03 +0000

    🖼🐓 Checkpoint ./public/bancoDeDados.html:38290350/126 ./public/scriptBancoDeDados.js:38290350/864

--- public/scriptBancoDeDados.js
+++ public/scriptBancoDeDados.js
@@ -10,12 +10,7 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "INSERT INTO ClientesSEBRAE (name, address) VALUES ('Company Inc', 'Highway 37')";
+  var sql = "INSERT INTO ClientesSEBRAE (nome, cpf, email, telefone, endereco) VALUES ('Company Inc', 'Highway 37', 'TEste', 'testando', 'apenas Teste')";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
