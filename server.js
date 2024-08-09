392ad63dd2f2fb163af183d17f7a5b7b2481cc21
Glitch (gerador-de-contratos-sebraetec) <none>
2024-08-09 18:39:27 +0000

    🍄⛰ Checkpoint ./public/scriptSebraetec.js:38290350/1684 ./server.js:38290350/4044

--- server.js
+++ server.js
@@ -1,35 +1,17 @@
 const https = require('https');
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configuração do banco de dados
-const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASS,
-  database: process.env.MYSQL_DB
-});
-
-db.connect((err) => {
-  if (err) throw err;
-  console.log("Database connected!");
-});
-
-// Middleware
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-// Rota para busca de CNPJ
 app.get('/cnpj/:cnpj', (req, res) => {
   const cnpj = req.params.cnpj;
   const options = {
     method: 'GET',
     hostname: 'receitaws.com.br',
+    port: null,
     path: `/v1/cnpj/${cnpj}`,
     headers: { Accept: 'application/json' }
   };
@@ -47,61 +29,10 @@ app.get('/cnpj/:cnpj', (req, res) => {
   apiReq.end();
 });
 
-// Rota principal
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Rota para adicionar dados ao banco de dados
-app.post('/addData', (req, res) => {
-  const { nome, cpf, email, tel, endereco } = req.body;
-  const query = 'INSERT INTO ClientesSEBRAE (nome, cpf, email, telefone, endereco) VALUES (?, ?, ?, ?, ?)';
-  
-  console.log('Dados recebidos:', { nome, cpf, email, tel, endereco });
-
-  db.query(query, [nome, cpf, email, tel, endereco], (err, result) => {
-    if (err) {
-      console.error('Erro ao inserir dados:', err);
-      res.status(500).send('Erro ao inserir dados: ' + err.message);
-      return;
-    }
-    console.log('Dados inseridos com sucesso:', result);
-    res.send('Dados adicionados ao banco de dados');
-  });
-});
-
-// Rota para buscar todos os cadastros
-app.get('/buscarCadastro', (req, res) => {
-  const query = 'SELECT nome, cpf, telefone FROM ClientesSEBRAE';
-  
-  db.query(query, (err, results) => {
-    if (err) {
-      console.error('Erro ao buscar dados:', err);
-      res.status(500).send('Erro ao buscar dados: ' + err.message);
-      return;
-    }
-    console.log('Dados encontrados:', results);
-    res.json(results);
-  });
-});
-
-// Rota para buscar dados por CPF
-app.get('/buscarPorCpf/:cpf', (req, res) => {
-  const cpf = req.params.cpf;
-  const query = 'SELECT * FROM ClientesSEBRAE WHERE cpf = ?';
-  
-  db.query(query, [cpf], (err, results) => {
-    if (err) {
-      console.error('Erro ao buscar dados:', err);
-      res.status(500).send('Erro ao buscar dados: ' + err.message);
-      return;
-    }
-    console.log('Dados encontrados:', results);
-    res.json(results);
-  });
-});
-
-// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
